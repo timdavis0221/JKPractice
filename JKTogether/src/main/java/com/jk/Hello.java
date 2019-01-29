993ad2d7606e8d3e33394933b53628e2e700e7fd
Tim Davis <timdavis10310@gmail.com>
2019-01-29 22:27:39 +0800

    play codility

--- JKTogether/src/main/java/com/jk/Hello.java
+++ JKTogether/src/main/java/com/jk/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         Person person = new Person();
         person.sayHello();
+        int[] getResult = person.getSumOfIntArray(1);
+        for (int num : getResult)
+            System.out.println(num);
     }
 }
--- JKTogether/src/main/java/com/jk/Person.java
+++ JKTogether/src/main/java/com/jk/Person.java
@@ -1,5 +1,7 @@
 package com.jk;
 
+import java.util.stream.IntStream;
+
 public class Person {
 
     public Person() {
@@ -8,6 +10,25 @@ public class Person {
 
     public void sayHello(){
         System.out.println("Hello Java !");
+    }
+
+    public int[] getSumOfIntArray(int N){
+
+        int[] result = new int[N];
+
+        if(N >= 1 && N <= 100){
+            for(int i = 0; i <= N - 1; i++){
+                if(i == N - 1)
+                    result[i] = -IntStream.of(result).sum();
+                else
+                    result[i] = i;
+            }
+        }
+        else{
+            System.out.println("input error");
+            result = new int[] { -1 };
+        }
+        return result;
     }
 
 }
