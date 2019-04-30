622d94d9f7c0b2100dd56a6fae98d5a2ba63ab4c
Tim Davis <timdavis10310@gmail.com>
2019-04-30 23:00:20 +0800

    finish CH 27 : use of HashMap and HashSet

--- JKTogether/src/main/java/com/jk/many/Tester.java
+++ JKTogether/src/main/java/com/jk/many/Tester.java
@@ -1,11 +1,41 @@
 package com.jk.many;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
+        // Map : pairs:<key, value>
+        HashMap<String, String> stocks = new HashMap<>();
+        stocks.put("2330", "TSMC");
+        stocks.put("2317", "FOXCONN");
+        stocks.put("2330", "台積電");
+        System.out.println(stocks);
+        System.out.println(stocks.get("2317"));
+        System.out.println("-----------------");
+        for (String key : stocks.keySet()) {
+            System.out.println(stocks.get(key));
+        }
+//        hashSet();
+//        list();
+    }
+
+    /*
+     * HashSet : no index, no repeat, no order
+     */
+    private static void hashSet() {
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(6);
+        hashSet.add(7);
+        hashSet.add(8);
+        hashSet.add(8);
+        hashSet.add(1);
+        System.out.println(hashSet);
+        for (int n : hashSet) {
+            System.out.println(n);
+        }
+    }
+
+    private static void list() {
         ArrayList<Integer> list = new ArrayList();
         list.add(5);
         list.add(2);
--- JKTogether/src/main/kotlin/com/jk/Many.kt
+++ JKTogether/src/main/kotlin/com/jk/Many.kt
@@ -1,7 +1,31 @@
 package com.jk
 
+import com.jk.kotlin.parking.Car
+import java.time.LocalDateTime
+
 fun main() {
-    val list = listOf(5, 1, 2, 4)
+    val parkingLot = HashMap<String, Car?>()
+
+    var enterTime = LocalDateTime.of(2019, 4, 30, 8, 0, 0)
+    var car:Car? = Car("AAA-0001", enterTime)
+    parkingLot.put(car!!.id, car)
+    car = Car("BBB-0002", enterTime.plusMinutes(15))
+    parkingLot.put(car.id, car)
+    // Car 1 leaving
+    var leaveTime = LocalDateTime.of(2019, 4, 30, 9, 0, 0)
+    car = parkingLot.get("AAA-0001")
+    car?.leave = leaveTime
+    println("${car?.id}, duration: ${car?.duration()} ")
+    parkingLot.remove(car?.id)
+    println(parkingLot.size)
+    // Car 2 leaving
+    car = parkingLot.get("BBB-0002")
+    car?.leave = leaveTime.plusHours(2)
+    println("${car?.id}, duration: ${car?.duration()}")
+    parkingLot.remove(car?.id)
+    println(parkingLot.size)
+
+/*   val list = listOf(5, 1, 2, 4)
     println(list)
     val scores = listOf(65, 44, 12, 85, 98)
     for (score in scores){
@@ -15,5 +39,5 @@ fun main() {
     println(mutableList)
 
     // Note: kotlin considered performance in change of mutable collection, so made the difference
-    // between immutable and mutable
+    // between immutable and mutable*/
 }
